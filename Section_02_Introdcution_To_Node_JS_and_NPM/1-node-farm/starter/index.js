c8bbf7315f6793cf4523694f2bb7dfac4e74b26b
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-05-23 11:01:49 +0600

    Routing

--- Section_02_Introdcution_To_Node_JS_and_NPM/1-node-farm/starter/index.js
+++ Section_02_Introdcution_To_Node_JS_and_NPM/1-node-farm/starter/index.js
@@ -1,10 +1,26 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 //SERVER
 const server = http.createServer((req, res) => {
-    // console.log(req);
-    res.end('Hello from the server!');
+    // console.log(req.url);
+    // res.end('Hello from the server!');
+    const pathName = req.url;
+
+    if(pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview');
+    }
+    else if (pathName === '/product') {
+        res.end('This is the PRODUCT');
+    } else {
+        // res.writeHead(404);
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello World'
+        });
+        res.end('<h1>Page can not be found</h1>');
+    }
 });
 
 server.listen(8000, '127.0.0.1', () => {
