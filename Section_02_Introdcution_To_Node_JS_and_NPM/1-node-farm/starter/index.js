159ce7760e78f9e45141bd5496881c24bdf054ba
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-05-23 11:31:06 +0600

    Building a Simple API - Part 1

--- Section_02_Introdcution_To_Node_JS_and_NPM/1-node-farm/starter/index.js
+++ Section_02_Introdcution_To_Node_JS_and_NPM/1-node-farm/starter/index.js
@@ -8,12 +8,23 @@ const server = http.createServer((req, res) => {
     // res.end('Hello from the server!');
     const pathName = req.url;
 
-    if(pathName === '/' || pathName === '/overview') {
+    if (pathName === '/' || pathName === '/overview') {
         res.end('This is the overview');
     }
     else if (pathName === '/product') {
         res.end('This is the PRODUCT');
-    } else {
+    }
+    else if (pathName === '/api') {
+        fs.readFile(`${__dirname}/dev-data/data.json`, 'utf-8', (err, data) => {
+            const productData = JSON.parse(data);
+            res.writeHead(200, {
+                'Content-type': 'application/json'
+            });
+            res.end(JSON.stringify(productData));
+        });
+
+    }
+    else {
         // res.writeHead(404);
         res.writeHead(404, {
             'Content-type': 'text/html',
