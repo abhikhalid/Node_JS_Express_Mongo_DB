d80d59a5b51765d30797a45dae5d0f61f2846d0b
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-05-23 16:58:40 +0600

    Using Modules 2: Our Own Modules

--- Section_02_Introdcution_To_Node_JS_and_NPM/1-node-farm/starter/index.js
+++ Section_02_Introdcution_To_Node_JS_and_NPM/1-node-farm/starter/index.js
@@ -2,28 +2,11 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const replaceTemplate = require('./modules/replaceTemplate');
+
 //top level code
 //gets executed once when the file is loaded
 
-const replaceTemplate = (temp, product) => {
-    //there might be multiple occurences of the same placeholder
-    //so we use regex to replace all occurences
-    //the g flag means global
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) {
-        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic'); //not-organic is a class name
-    }
-
-    return output;
-}
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
---
+++ Section_02_Introdcution_To_Node_JS_and_NPM/1-node-farm/starter/modules/replaceTemplate.js
@@ -0,0 +1,19 @@
+module.exports = (temp, product) => {
+    //there might be multiple occurences of the same placeholder
+    //so we use regex to replace all occurences
+    //the g flag means global
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) {
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic'); //not-organic is a class name
+    }
+
+    return output;
+}
