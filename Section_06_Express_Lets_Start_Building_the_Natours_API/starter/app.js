446731afebb4dafae27e288dd67c8e61072e8f77
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-05-24 15:14:44 +0600

    Refactoring Our Routes

--- Section_06_Express_Lets_Start_Building_the_Natours_API/starter/app.js
+++ Section_06_Express_Lets_Start_Building_the_Natours_API/starter/app.js
@@ -10,24 +10,13 @@ const app = express();
 app.use(express.json()); // Middleware to parse JSON data
 
 
-// app.get('/', (req, res) => {
-//     // res.status(200).send('Hello from the server side!');
-//     res.status(200).json({
-//         message: 'Hello from the server side!',
-//         app: 'Natours',
-//     });
-// });
-
-// app.post('/', (req, res) => {
-//     res.send('You can post to this endpoint');
-// });
-
 //__dirname => current directory of where this file is located
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+
+const getAllTours = (req, res) => {
     res.status(200).json({
         status: 'success',
         results: tours.length,
@@ -35,10 +24,9 @@ app.get('/api/v1/tours', (req, res) => {
             tours: tours,
         }
     });
-});
+};
 
-
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
     console.log(req.params); // req.params is an object that contains the route parameters. In this case, it will contain the id parameter.
 
     const id = req.params.id * 1; // convert the string to a number
@@ -59,27 +47,9 @@ app.get('/api/v1/tours/:id', (req, res) => {
             tour: tour // +req.params.id converts the string to a number
         }
     });
-});
+};
 
-
-app.post('/api/v1/tours', (req, res) => {
-    // console.log(req.body); // body is the available now because we used that middleware.
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body); // create a new object with the newId and the body of the request.
-    tours.push(newTour); // push the new tour to the array.
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour,
-            }
-        });
-    }); // why async function?  why not just use fs.writeFileSync? because we want to make sure that the server is not blocked while writing the file. We want to be able to handle other requests while this one is being processed. We are inside a callback function that is gonna run in the event loop. 
-
-    // res.send('Done'); //we always need to send back something in order to finish request-response cycle.
-});
-
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
     const id = req.params.id * 1; // convert the string to a number
     if (id > tours.length) {
         return res.status(404).json({
@@ -99,10 +69,26 @@ app.patch('/api/v1/tours/:id', (req, res) => {
             }
         });
     });
-});
+};
 
+const createTour = (req, res) => {
+    // console.log(req.body); // body is the available now because we used that middleware.
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body); // create a new object with the newId and the body of the request.
+    tours.push(newTour); // push the new tour to the array.
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour,
+            }
+        });
+    }); // why async function?  why not just use fs.writeFileSync? because we want to make sure that the server is not blocked while writing the file. We want to be able to handle other requests while this one is being processed. We are inside a callback function that is gonna run in the event loop. 
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+    // res.send('Done'); //we always need to send back something in order to finish request-response cycle.
+};
+
+const deleteTour = (req, res) => {
     const id = req.params.id * 1; // convert the string to a number
     if (id > tours.length) {
         return res.status(404).json({
@@ -120,7 +106,29 @@ app.delete('/api/v1/tours/:id', (req, res) => {
             data: null,
         });
     });
-});
+};
+
+// app.get('/api/v1/tours', getAllTours);
+
+
+// app.get('/api/v1/tours/:id', getTour);
+
+
+// app.post('/api/v1/tours', createTour);
+
+// app.patch('/api/v1/tours/:id', updateTour);
+
+
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours')
+    .get(getAllTours)
+    .post(createTour);
+
+app.route('/api/v1/tours/:id')
+    .get(getTour)
+    .patch(updateTour)
+    .delete(deleteTour);
 
 
 const port = 3000;
