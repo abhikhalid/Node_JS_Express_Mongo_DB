25abb2890df853fcc2fe9fa07c971724d672c029
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-05-25 13:48:26 +0600

    Modelling the Tours

--- Section_08_Using_MongoDB_with_Mongoose/starter/controllers/tourController.js
+++ Section_08_Using_MongoDB_with_Mongoose/starter/controllers/tourController.js
@@ -104,7 +104,7 @@ exports.createTour = async (req, res) => {
     catch (err) {
         res.status(400).json({
             status: 'fail',
-            message: err.message // return the error message
+            message: err // return the error message
         });
     }
 };
--- Section_08_Using_MongoDB_with_Mongoose/starter/models/tourModel.js
+++ Section_08_Using_MongoDB_with_Mongoose/starter/models/tourModel.js
@@ -6,18 +6,55 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, "A tour must have a name"],
         unique: true,
+        trim: true, // remove whitespace from the beginning and end of the string
     },
-    rating: {
+    duration: {
+        type: Number,
+        required: [true, "A tour must have a duration"],
+    },
+    maxGroupSize: {
+        type: Number,
+        required: [true, "A tour must have a group size"],
+    },
+    difficulty: {
+        type: String,
+        required: [true, "A tour must have a difficulty"],
+
+    },
+    ratingsAverage: {
         type: Number,
         default: 4.5,
-        min: [1, "Rating must be above 1.0"],
-        max: [5, "Rating must be below 5.0"],
+    },
+    ratingsQuantity: {
+        type: Number,
+        default: 0,
     },
     // price: Number,
     price: {
         type: Number,
         required: [true, "A tour must have a price"],
     },
+    priceDiscount: Number,
+    summary: {
+        type: String,
+        trim: true, // remove whitespace from the beginning and end of the string
+        required: [true, "A tour must have a summary"],
+    },
+    description: {
+        type: String,
+        trim: true, // remove whitespace from the beginning and end of the string
+    },
+    imageCover: {
+        type: String,
+        required: [true, "A tour must have a cover image"],
+    },
+    images: [String], // array of strings
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+        // select: false, // do not return this field in the response
+    },
+    startDates: [Date], // array of dates
 });
 
 //we have defined a schema, now we need to create a model
