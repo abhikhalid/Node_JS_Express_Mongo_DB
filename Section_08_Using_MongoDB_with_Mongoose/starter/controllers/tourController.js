3509e12837133b0776fbdb1ec1649da99679e1bf
abhikhalid <98512518+abhikhalid@users.noreply.github.com>
2025-05-25 20:39:48 +0600

    Making the API Better: Sorting

--- Section_08_Using_MongoDB_with_Mongoose/starter/controllers/tourController.js
+++ Section_08_Using_MongoDB_with_Mongoose/starter/controllers/tourController.js
@@ -38,20 +38,33 @@ exports.getAllTours = async (req, res) => {
         console.log(req.query); // log the query object to see what fields are being queried')
 
 
-        //1) Filtering
+        //1A) Filtering
         const queryObj = { ...req.query }; // create a copy of the query object
         const excludedFields = ['page', 'sort', 'limit', 'fields']; // fields to exclude from the query
         excludedFields.forEach(el => delete queryObj[el]); // remove the excluded fields from the query object
 
         // console.log(req.query, queryObj); // log the query object to see what fields are being queried
 
-        //2) Advanced Filtering
+        //1B) Advanced Filtering
         let queryStr = JSON.stringify(queryObj); // convert the query object to a string
         queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`); // replace all instances of gte, gt, lte, lt with $gte, $gt, $lte, $lt
 
-        console.log(JSON.parse(queryStr)); // log the query string to see what fields are being queried
+        // console.log(JSON.parse(queryStr)); // log the query string to see what fields are being queried
 
-        const query = Tour.find(JSON.parse(queryStr)); // find all tours using the Tour model
+        let query = Tour.find(JSON.parse(queryStr)); // find all tours using the Tour model
+
+        //2) Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' '); // split the sort query by comma and join with space
+            // console.log(sortBy); // log the sort query to see what fields are being sorted
+            query = query.sort(sortBy); // sort the query by the fields specified in the sort query
+            //sort('price ratingAverage') // sort by price and ratingAverage
+        } else {
+            //if user does not specify sort query, sort by createdAt in descending order
+            query = query.sort('-createdAt'); // default sort by createdAt in descending order
+        }
+
+        //EXECUTE QUERY
         const tours = await query; // execute the query and get the tours
 
         res.status(200).json({
